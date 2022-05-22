5377864b3a394a72212a3a5f21cebd19ca468fe0
Flora K <akaliza@gmail.com>
2022-05-22 15:57:23 -0400

    lps and kmp algo pattern and string matching

---
+++ src/main/java/string/LPS.java
@@ -0,0 +1,41 @@
+package string;
+
+import java.util.Arrays;
+
+public class LPS {
+    /*
+    LPS : longest Prefix Suffix
+    Time : O(N)
+     */
+    public static void main(String[] args) {
+
+        String s = "AABAACAADAABAABA";
+        int[] LPS = compute(s);
+        System.out.println(Arrays.toString(LPS));
+    }
+
+    public static  int[] compute(String s) {
+        int i = 1;
+        int len = 0;
+        int n = s.length();
+
+        int[] LPS = new int[n];
+
+        while (i < n) {
+            if (s.charAt(i) == s.charAt(len)){
+                LPS[i] =  len+1;
+                i++;
+                len++;
+            }else{
+                if(len!=0){
+                    len = LPS[len-1];
+                }else {
+                    LPS[i]=0;
+                    i++;
+                }
+            }
+
+        }
+        return LPS;
+    }
+}
---
+++ src/main/java/string/Z_Algo.java
@@ -0,0 +1,2 @@
+package string;public class Z_Algo {
+}
